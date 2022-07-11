696de7f8f41775794665a700b75a9a8a01409010
Ahmadreza Mozaffary <79336004+AhmadrezaMozaffary@users.noreply.github.com>
2022-07-11 09:52:46 +0430

    Dynamic navigation using useHistory hook

--- src/pages/NewQoute.js
+++ src/pages/NewQoute.js
@@ -1,11 +1,16 @@
+import { useHistory } from "react-router-dom";
 import QouteForm from "../components/quotes/QuoteForm";
 
 const NewQoute = () => {
+  const history = useHistory();
+
   const addQouteData = (qouteData) => {
     console.log(qouteData);
+
+    history.replace("/qoutes");
   };
 
-  return <QouteForm onAddQoute={addQouteData} />;
+  return <QouteForm onAddQuote={addQouteData} />;
 };
 
 export default NewQoute;
