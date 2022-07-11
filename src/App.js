a6397e4087b6aa28bdc34066c16d4bc106796e26
Ahmadreza Mozaffary <79336004+AhmadrezaMozaffary@users.noreply.github.com>
2022-07-11 15:28:54 +0430

    Lazy loading and partialy download

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
+import React, { Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
-import Layout from "./components/layout/Layout";
 
 // Pages 📑
 import AllQoutes from "./pages/AllQuotes";
-import NewQoute from "./pages/NewQuote";
-import NotFound from "./pages/NotFound";
-import QouteDetail from "./pages/QuoteDetail";
+import Layout from "./components/layout/Layout";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
-        <Route path="/quotes" exact>
-          <AllQoutes />
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QouteDetail />
-        </Route>
-        <Route path="/new-quote">
-          <NewQoute />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQoutes />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+          <Route path="/new-quote">
+            <NewQuote />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
